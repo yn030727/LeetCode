27b39eb548c8694fe8d157d5c67a3c97f2930217
yn030727 <2601098388@qq.com>
2023-01-20 15:01:55 +0800

    1.Day9

---
+++ src/DayTarget.java
@@ -0,0 +1,29 @@
+public class DayTarget {
+    int Image;
+    String description;
+    String day;
+
+    public int getImage() {
+        return Image;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImage(int image) {
+        Image = image;
+    }
+}
---
+++ src/LeetCode2299.java
@@ -0,0 +1,48 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode2299 {
+    //解题思路：模拟，
+    public boolean strongPasswordCheckerII(String password) {
+        //先判断长度
+        if (password.length() < 8) {
+            return false;
+        }
+
+        Set<Character> specials = new HashSet<Character>() {{
+            add('!');
+            add('@');
+            add('#');
+            add('$');
+            add('%');
+            add('^');
+            add('&');
+            add('*');
+            add('(');
+            add(')');
+            add('-');
+            add('+');
+        }};
+        int n = password.length();
+        boolean hasLower = false, hasUpper = false, hasDigit = false, hasSpecial = false;
+        for (int i = 0; i < n; ++i) {
+            //不能出现同样的字符
+            if (i != n - 1 && password.charAt(i) == password.charAt(i + 1)) {
+                return false;
+            }
+            //判断题目要求的四个条件
+            char ch = password.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                hasLower = true;
+            } else if (Character.isUpperCase(ch)) {
+                hasUpper = true;
+            } else if (Character.isDigit(ch)) {
+                hasDigit = true;
+            } else if (specials.contains(ch)) {
+                hasSpecial = true;
+            }
+        }
+
+        return hasLower && hasUpper && hasDigit && hasSpecial;
+    }
+}
