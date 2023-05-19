3db0f15126b0a27b50529b360a0849e601f70a3d
yn030727 <2601098388@qq.com>
2023-05-19 14:39:57 +0800

    1.Day31——JVM OOM

--- src/OOM.java
+++ src/OOM.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class OOM {
 
+    static class OOMObject{}
 
     public static void main(String[] args) {
 
+        List<OOMObject> list = new ArrayList<>();
+
+        while(true){
+            list.add(new OOMObject());
+        }
     }
 }
