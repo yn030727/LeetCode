b2f73a963c3ba02c9f51b1224297346ab428cb71
yn030727 <2601098388@qq.com>
2023-05-20 09:52:53 +0800

    1.Day31——JVM OOM

--- src/RuntimeConstantPoolOOM.java
+++ src/RuntimeConstantPoolOOM.java
@@ -4,17 +4,34 @@ import java.util.Set;
 public class RuntimeConstantPoolOOM {
 
     public static void main(String[] args) {
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1.intern() == str1);
 
-        //使用Set保持常量池的引用，避免Full GC回收常量池的行为
-        Set<String> set = new HashSet<>();
-
-        //在short范围内足以让6MB的PermSize产生OOM了
-        short i = 0;
-
-        while(true){
-            set.add(String.valueOf(i++).intern());
-        }
-
+        String str2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str2.intern() == str2);
     }
-
 }
+
+
+
+
+
+
+
+//public class RuntimeConstantPoolOOM {
+//
+//    public static void main(String[] args) {
+//
+//        //使用Set保持常量池的引用，避免Full GC回收常量池的行为
+//        Set<String> set = new HashSet<>();
+//
+//        //在short范围内足以让6MB的PermSize产生OOM了
+//        short i = 0;
+//
+//        while(true){
+//            set.add(String.valueOf(i++).intern());
+//        }
+//
+//    }
+//
+//}
