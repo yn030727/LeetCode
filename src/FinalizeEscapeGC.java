b7a0e6072aab38970ae674a1507b6e63a7927922
yn030727 <2601098388@qq.com>
2023-05-23 05:46:44 +0800

    1.Day31——JVM OOM

---
+++ src/FinalizeEscapeGC.java
@@ -0,0 +1,43 @@
+public class FinalizeEscapeGC {
+
+    public static FinalizeEscapeGC SAVE_HOOK = null;
+
+    public void isAlive(){
+        System.out.println("yes , i am still alive");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("finalize method executed!");
+        FinalizeEscapeGC.SAVE_HOOK = this;
+    }
+
+    public static void main(String[] args)  throws Throwable{
+
+        SAVE_HOOK = new FinalizeEscapeGC();
+
+        //对象第一次成功拯救自己
+        SAVE_HOOK = null;
+        System.gc();
+        //因为Finalizer方法优先级很低，暂停0.5s,以等待它。
+        Thread.sleep(500);
+        if( SAVE_HOOK != null){
+            SAVE_HOOK.isAlive();
+        }else{
+            System.out.println("no , i am dead");
+        }
+
+
+        //第二次自救失败
+        SAVE_HOOK = null;
+        System.gc();
+        //因为Finalizer方法优先级很低、暂停0.5秒，以等待它
+        Thread.sleep(500);
+        if(SAVE_HOOK != null){
+            SAVE_HOOK.isAlive();
+        }else{
+            System.out.println("no , i am dead");
+        }
+    }
+}
