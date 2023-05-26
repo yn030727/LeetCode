b795f91b4ff1a00bbf8e520b0f89aa5ecbb4ebd5
yn030727 <2601098388@qq.com>
2023-05-26 14:02:30 +0800

    1.Day31——JVM OOM

--- src/testAll.java
+++ src/testAll.java
@@ -3,7 +3,7 @@ public class testAll {
 
 
     public static void main(String[] args) {
-        testAllocation();
+        testTenuringThreshold();
     }
 
     public static void testAllocation(){
@@ -12,5 +12,22 @@ public class testAll {
         allocation2 = new byte[2 * _1MB];
         allocation3 = new byte[2 * _1MB];
         allocation4 = new byte[4 * _1MB];  //出现一次Minor GC
+    }
+
+
+    public static void testPretenureSizeThreshold(){
+        byte[] allocation;
+        allocation = new byte[4 * _1MB];  //直接分配在老年代
+    }
+
+    public static void testTenuringThreshold(){
+        byte[] allocation1 , allocation2 , allocation3 ;
+
+        allocation1 = new byte[_1MB / 4];
+
+        allocation2 = new byte[4 * _1MB];
+        allocation3 = new byte[4 * _1MB];
+        allocation3 = null;
+        allocation3 = new byte[4 * _1MB];
     }
 }
