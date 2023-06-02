d653fed19a1c216cadc57f3e7fb625b32bc06a1d
yn030727 <2601098388@qq.com>
2023-06-02 15:57:06 +0800

    1.Day36——JVM

---
+++ src/ConstClass.java
@@ -0,0 +1,7 @@
+public class ConstClass {
+    static {
+        System.out.println("ConstClass init!");
+    }
+
+    public static final String HELLOWORLD = "hello world";
+}
--- src/NotInitialization.java
+++ src/NotInitialization.java
@@ -1,5 +1,6 @@
 public class NotInitialization {
     public static void main(String[] args) {
-        System.out.println(SubClass.value) ;
+        SuperClass[]  sca = new SuperClass[10];
+        System.out.println(ConstClass.HELLOWORLD);
     }
 }
