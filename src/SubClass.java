e506501881d20efeafcc21706f219aaf046ef510
yn030727 <2601098388@qq.com>
2023-06-02 12:35:55 +0800

    1.Day35——LeetCode Offer

---
+++ src/SubClass.java
@@ -0,0 +1,5 @@
+public class SubClass extends SuperClass{
+    static {
+        System.out.println("SubClass init!");
+    }
+}
---
+++ src/SuperClass.java
@@ -0,0 +1,7 @@
+public class SuperClass {
+    static {
+        System.out.println("SuperClass init!");
+    }
+    public static int value = 123;
+}
+
