c9e864abc13b7a9ec2cf0a1fe315e8cf81823d40
yn030727 <2601098388@qq.com>
2023-06-03 09:01:46 +0800

    1.Day36——JVM

---
+++ src/LocalVariablesTableDemo.java
@@ -0,0 +1,8 @@
+public class LocalVariablesTableDemo {
+    public static void main(String[] args) {
+        {
+            byte[] placeholder = new byte[64 * 1024 * 1024];
+        }
+        System.gc();
+    }
+}
