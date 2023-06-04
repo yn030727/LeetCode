bdc277c4f454fbbd3b8e1b63cfeb77de935ae6df
yn030727 <2601098388@qq.com>
2023-06-04 18:35:49 +0800

    1.Day39-JVM

--- src/TypeErasureGenerics.java
+++ src/TypeErasureGenerics.java
@@ -1,5 +1,9 @@
-public class TypeErasureGenerics<E>{
-    public void doSomething(Object item){
-
-    }
-}
+//public class TypeErasureGenerics<E>{
+//    public void doSomething(Object item){
+//        if(item instanceof E){ //不合法，无法对泛型进行实例判断
+//            ...
+//        }
+//        E newItem = new E(); //不合法，无法使用泛型创建对象
+//        E[] itemArray = new E[10]; //不合法，无法使用泛型创建数组
+//    }
+//}
