09fa368cc7a65b75331fc741fafdde0b83c21a23
yn030727 <2601098388@qq.com>
2023-06-06 16:57:09 +0800

    1.Day39-JVM

--- src/Compilation/BasicDemo.java
+++ src/Compilation/BasicDemo.java
@@ -17,4 +17,30 @@ public class BasicDemo {
         z = b.get();
         sum = y + z;
     }
+
+    //冗杂访问消除
+    public void foo1(){
+        y = b.value;
+        //...
+        z = y;
+        sum = y + z;
+    }
+
+    //复写传播
+    public void foo2(){
+        y = b.value;
+        //...
+        y = y;
+        sum = y + y;
+    }
+
+    public static void foo(Object object){
+        if(object != null){
+            System.out.println("do something");
+        }
+    }
+    public static void testInline(String[] args){
+        Object obj = null;
+        foo(obj);
+    }
 }
