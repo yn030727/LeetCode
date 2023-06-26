9028674ca4ea3079e306e27bac12e361afb9f7d4
yn030727 <2601098388@qq.com>
2023-06-26 21:59:12 +0800

    1.Day40-Offer

---
+++ src/Compilation/writeReadProblem.java
@@ -0,0 +1,5 @@
+package Compilation;
+
+public class writeReadProblem {
+
+}
---
+++ src/Offer21.java
@@ -0,0 +1,6 @@
+public class Offer21 {
+    public int[] exchange(int[] nums){
+        int left = 0 , right = nums.length;
+    }
+
+}
