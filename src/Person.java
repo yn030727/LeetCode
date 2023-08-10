b8abc1d32918b70c1a93bc7f1eaa731d557a9cf8
yn030727 <2601098388@qq.com>
2023-08-10 13:18:35 +0800

    1.Day45-Offer

---
+++ src/Person.java
@@ -0,0 +1,24 @@
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    public static final long serialVersionUID = 231412412351515L;
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
